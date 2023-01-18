aeaf583370ae61ef1ec8db8b0cef7d4481591605
ManuelGenaro01 <manuelgenaro19@gmail.com>
2023-01-18 19:32:10 -0300

    ToDo Drag Drop

--- src/components/modules/TodoContent.jsx
+++ src/components/modules/TodoContent.jsx
@@ -1,17 +1,52 @@
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
 import TodoItems from "./TodoItems";
 
 const TodoContent = ({ todos, removeTodo, completeTodo }) => {
+    const handleDragEnd = (result) => {
+        if (!result.destination) return;
+        const startIndex = result.source.index;
+        const endIndex = result.destination.index;
+
+        const copyArray = [...todos];
+        const [removeItem] = copyArray.splice(startIndex, 1);
+        copyArray.splice(endIndex, 0, removeItem);
+
+        todos = copyArray;
+    };
+
     return (
-        <div className="mt-8 rounded-t-md bg-white text-xs shadow-3xl transition-all duration-700 dark:bg-gray-800">
-            {todos.map((todo) => (
-                <TodoItems
-                    key={todo.id}
-                    todo={todo}
-                    removeTodo={removeTodo}
-                    completeTodo={completeTodo}
-                />
-            ))}
-        </div>
+        <DragDropContext onDragEnd={handleDragEnd}>
+            <div className="mt-8 rounded-t-md bg-white text-xs shadow-3xl transition-all duration-700 dark:bg-gray-800">
+                <Droppable droppableId="todos">
+                    {(droppableProvider) => (
+                        <div
+                            ref={droppableProvider.innerRef}
+                            {...droppableProvider.droppableProps}
+                        >
+                            {todos.map((todo, index) => (
+                                <Draggable
+                                    index={index}
+                                    draggableId={`${todo.id}`}
+                                    key={todo.id}
+                                >
+                                    {(draggableProvider) => (
+                                        <TodoItems
+                                            todo={todo}
+                                            removeTodo={removeTodo}
+                                            completeTodo={completeTodo}
+                                            ref={draggableProvider.innerRef}
+                                            {...draggableProvider.dragHandleProps}
+                                            {...draggableProvider.draggableProps}
+                                        />
+                                    )}
+                                </Draggable>
+                            ))}
+                            {droppableProvider.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </div>
+        </DragDropContext>
     );
 };
 
--- src/components/modules/TodoItems.jsx
+++ src/components/modules/TodoItems.jsx
@@ -1,36 +1,43 @@
 import Check from "../images/Check";
 import Icon from "../images/Icon";
+import React from "react";
 
-const TodoItems = ({ todo, removeTodo, completeTodo }) => {
-    const { id, nombre, completed } = todo;
-    return (
-        <article className="flex items-center gap-4 border-b border-b-gray-300 py-4 px-4">
-            <button
-                className={`h-5 w-5 flex-none rounded-full border-2  ${
-                    completed
-                        ? "flex items-center justify-center rounded-full border-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
-                        : "inline-block "
-                }`}
-                onClick={() => {
-                    completeTodo(id);
-                }}
+const TodoItems = React.forwardRef(
+    ({ todo, removeTodo, completeTodo, ...props }, ref) => {
+        const { id, nombre, completed } = todo;
+        return (
+            <article
+                {...props}
+                ref={ref}
+                className="flex items-center gap-4 border-b border-b-gray-300 py-4 px-4"
             >
-                {completed && <Check />}
-            </button>
-            <p
-                className={`grow ${
-                    completed
-                        ? "text-gray-400 line-through"
-                        : "text-black transition-all duration-700 dark:text-white"
-                }`}
-            >
-                {nombre}
-            </p>
-            <button className="flex-none" onClick={() => removeTodo(id)}>
-                <Icon />
-            </button>
-        </article>
-    );
-};
+                <button
+                    className={`h-5 w-5 flex-none rounded-full border-2  ${
+                        completed
+                            ? "flex items-center justify-center rounded-full border-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+                            : "inline-block "
+                    }`}
+                    onClick={() => {
+                        completeTodo(id);
+                    }}
+                >
+                    {completed && <Check />}
+                </button>
+                <p
+                    className={`grow ${
+                        completed
+                            ? "text-gray-400 line-through"
+                            : "text-black transition-all duration-700 dark:text-white"
+                    }`}
+                >
+                    {nombre}
+                </p>
+                <button className="flex-none" onClick={() => removeTodo(id)}>
+                    <Icon />
+                </button>
+            </article>
+        );
+    }
+);
 
 export default TodoItems;
